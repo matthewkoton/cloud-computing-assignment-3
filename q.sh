28f8c35e4b2d0d806e9a0adec3af5fb4167334a3
Matthew <matthewbkoton100@gmail.com>
2023-06-21 19:32:52 +0300

    k

--- q.sh
+++ q.sh
@@ -9,10 +9,9 @@ while IFS= read -r name; do
     curl --location --request POST "http://127.0.0.1:8000/dishes" -H 'Content-Type: application/json' -d '{"name":"'${name}'"}'
     response = $(curl --location --request GET "http://127.0.0.1:8000/dishes/${counter}")
 
-
-    cal=$(echo $response | jq -r '.cal')
-    sodium=$(echo $response | jq -r '.sodium')
-    sugar=$(echo $response | jq -r '.sugar')
+    cal = "${response}"| jq -r '.cal'
+    sodium = "${response}"| jq -r '.sodium'
+    sugar = "${response}"| jq -r '.sugar'
 
     echo "${name} contains (${cal}) calories, (${sodium}) mgs of sodium and (${sugar}) grams of sugar" >> mypath/response
     
