3343fcc4e0228bceadecfb0c033b46c956d0a264
Matthew <matthewbkoton100@gmail.com>
2023-06-21 18:43:14 +0300

    fixing paths 4

--- b/q.sh
+++ b/q.sh
@@ -3,11 +3,11 @@ while IFS= read -r name; do
     name=$(echo "$name" | xargs)
     
     curl --location --request POST "http://127.0.0.1:8000/${counter}" -H 'Content-Type: application/json' -d '{"name":"'${name}'"}'
-    curl --location --request GET "http://127.0.0.1:8000/${counter}" >> mypath/response.txt
+    curl --location --request GET "http://127.0.0.1:8000/${counter}" >> ./mypath/response.txt
     
     counter=$((counter+1))
 
 done < ./queries.txt
 
 echo "#########"
-cat mypath/response.txt
+cat ./mypath/response.txt
