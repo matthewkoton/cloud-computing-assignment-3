f0f4ebdc7851e7606a76f0bfe999bde40ba79b29
Matthew <matthewbkoton100@gmail.com>
2023-06-21 19:07:58 +0300

    formatting bash

--- q.sh
+++ q.sh
@@ -5,8 +5,16 @@ while IFS= read -r name; do
     echo "#########"
     echo $counter
     curl --location --request POST "http://127.0.0.1:8000/dishes" -H 'Content-Type: application/json' -d '{"name":"'${name}'"}'
-    curl --location --request GET "http://127.0.0.1:8000/dishes/${counter}" >> mypath/response
+    response = curl --location --request GET "http://127.0.0.1:8000/dishes/${counter}" >> mypath/response
     
+    cal=$(echo $response | jq -r '.cal')
+    sodium=$(echo $response | jq -r '.sodium')
+    sugar=$(echo $response | jq -r '.sugar')
+
+    echo "${name} contains (${cal}) calories, (${sodium}) mgs of sodium and (${sugar}) grams of sugar" >> mypath/response
+    
+
+
     counter=$((counter+1))
 
 done < queries.txt
