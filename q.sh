88141dc60524ebdd6345ecf92bbf342b9c222654
Matthew <matthewbkoton100@gmail.com>
2023-06-21 19:14:26 +0300

    debug 2

--- q.sh
+++ q.sh
@@ -4,10 +4,12 @@ while IFS= read -r name; do
     
     echo "#########"
     echo $counter
+    echo $name
+
     curl --location --request POST "http://127.0.0.1:8000/dishes" -H 'Content-Type: application/json' -d '{"name":"'${name}'"}'
     response = curl --location --request GET "http://127.0.0.1:8000/dishes/${counter}" >> mypath/response
-    
-    cat $(response)
+
+    echo $response
 
     cal=$(echo $response | jq -r '.cal')
     sodium=$(echo $response | jq -r '.sodium')
