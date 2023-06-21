980c8339c3a1d380629a92b74356c497575229be
Matthew <matthewbkoton100@gmail.com>
2023-06-21 18:56:06 +0300

    debug

--- q.sh
+++ q.sh
@@ -2,6 +2,8 @@ counter=1
 while IFS= read -r name; do
     name=$(echo "$name" | xargs)
     
+    echo "#########"
+    echo $counter
     curl --location --request POST "http://127.0.0.1:8000/dishes" -H 'Content-Type: application/json' -d '{"name":"'${name}'"}'
     curl --location --request GET "http://127.0.0.1:8000/dishes/${counter}" >> mypath/response
     
