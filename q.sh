b246a2cf602ae162e3779fd8cc7ee1358d57c95f
Matthew <matthewbkoton100@gmail.com>
2023-06-22 13:55:44 +0300

    fixing curls spaces

--- q.sh
+++ q.sh
@@ -2,9 +2,9 @@ counter=1
 while IFS= read -r name; do
     name=$(echo "$name" | xargs)
     
-    curl --location --request POST "http://127.0.0.1:8000/dishes" -H 'Content-Type: application/json' -d '{"name":"'${name}'"}'
+    #curl --location --request POST "http://127.0.0.1:8000/dishes" -H 'Content-Type: application/json' -d '{"name":"'${name}'"}'
+    curl --location --request POST "http://127.0.0.1:8000/dishes" -H 'Content-Type: application/json' -d "{\"name\":\"${name}\"}"
 
-    echo "--location --request POST "http://127.0.0.1:8000/dishes" -H 'Content-Type: application/json' -d '{"name":"'${name}'"}'"
     response=$(curl --location --request GET "http://127.0.0.1:8000/dishes/${counter}")
     
     cal=$(echo "$response" | jq -r '.cal')
