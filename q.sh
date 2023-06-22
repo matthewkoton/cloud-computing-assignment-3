087fa03796f9bf5d0723f97006ee6ddf02ca896f
Matthew <matthewbkoton100@gmail.com>
2023-06-22 13:51:06 +0300

    fix

--- q.sh
+++ q.sh
@@ -3,6 +3,8 @@ while IFS= read -r name; do
     name=$(echo "$name" | xargs)
     
     curl --location --request POST "http://127.0.0.1:8000/dishes" -H 'Content-Type: application/json' -d '{"name":"'${name}'"}'
+
+    echo "--location --request POST "http://127.0.0.1:8000/dishes" -H 'Content-Type: application/json' -d '{"name":"'${name}'"}'"
     response=$(curl --location --request GET "http://127.0.0.1:8000/dishes/${counter}")
     
     cal=$(echo "$response" | jq -r '.cal')
